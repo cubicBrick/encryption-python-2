d56acf4574f1fefbef2a3f05736fde1feeb91c5b
JaydenHor <jaydenhor@outlook.com>
2024-11-01 13:14:57 -0700

    Added initial rsa

--- encryption/rsa/core.py
+++ encryption/rsa/core.py
@@ -1,1 +1,22 @@
 import type
+
+
+class RSA:
+    def __encryptRawOnce__(data: int, publickey: type.publicRSAkey) -> int:
+        return pow(data, publickey.e, publickey.n)
+
+    def __decryptRawOnce__(data: int, privatekey: type.privateRSAkey) -> int:
+        return pow(data, privatekey.d, privatekey.n)
+
+    def __encryptRaw__(
+        data: type.__rawData__, publickey: type.publicRSAkey
+    ) -> type.__rawEncrypted__:
+        res: "type.__rawEncrypted__"
+        for i in data:
+            res.append(RSA.__decryptRawOnce__(i, publickey))
+        return res
+    
+    def __decyptRawUnchecked__(
+            data: type.__rawEncrypted__, privatekey: type.privateRSAkey
+    ) -> type.__rawData__:
+        res: "type.__rawData__"
--- encryption/rsa/type.py
+++ encryption/rsa/type.py
@@ -1,7 +1,33 @@
 from multipledispatch import dispatch
 from sympy import randprime
 
+class __rawData__(list[int]):
+    pass
+
+class __rawEncrypted__(list[int]):
+    pass
+
+class publicRSAkey:
+    def __init__(self, n: int, e: int):
+        self.n = n
+        self.e = e
+
+class privateRSAkey:
+    def __init__(self, n: int, d: int, e: int):
+        self.n = n
+        self.d = d
+        self.e = e
+    def public(self) -> publicRSAkey:
+        return publicRSAkey(self.e, self.e)
+
+
 class fullRSAkey:
+    n: "int"
+    e: "int"
+    d: "int"
+    ct: "int"
+    p: "int"
+    q: "int"
     @dispatch()
     # Initilize blank key.
     def __init__(self):
@@ -9,8 +35,10 @@ class fullRSAkey:
     @dispatch(int)
     # Initilize key from random size (2**sz) bits
     def __init__(self, sz: int):
-        self.__init__(randprime(1, 2**sz), randprime(1, 2**sz))
+        self = fullRSAkey(randprime(1, 2**sz), randprime(1, 2**sz))
     @dispatch(int, int)
     # Initilize key from p and q
     def __init__(self, p: int, q: int):
         pass
+    def private(self) -> privateRSAkey:
+        return privateRSAkey(self.n, self.d, self.e)
